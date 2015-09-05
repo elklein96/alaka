226f293e1b3c47a78a59d1b464907aeb4dee68d3
elklein96 <elklein96@gmail.com>
2015-09-05 08:33:09 -0400

    aws

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var server = http.createServer(app);
 
 app.use(express.static(__dirname + '/public'));
 
-server.listen(process.env.PORT || app.get('port'), function(){
+server.listen(process.env.PORT || 80, function(){
   console.log("Express server listening on port %d", this.address().port);
 });
 
