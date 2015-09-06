5422622dae0a5a10f8aaab1f2b077d5b67f11e62
elklein96 <elklein96@gmail.com>
2015-09-06 01:59:14 -0400

    Sorry, I really am that stupid. It is decided.

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ app.post('/parse', function(req, res) {
   //console.log(req.body);
   var face = req.body.face;
   var result = [];
-  face = toBuffer(face);
+  face = toBuffer(new Uint32Array(face));
 
-  fs.write('./public/cache/image'+generateRandomString(8), face+"", function(err){
+  fs.write('./public/cache/image'+generateRandomString(8), face, function(err){
     console.log(err);
   });
   
